8de9a8d7286d990ae97a3f2e0fddcbecd061f411
ashugeo <hugo.saintemarie@gmail.com>
2021-06-24 10:27:18 +0200

    More logic and styles

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ $(document).on('click', '[data-to-section]', e => {
     $el.addClass('selected');
 
     const to = $el.attr('data-to-section');
-    $('[data-section]').hide();
-    $(`[data-section="${to}"]`).show();
+    $('[data-section]').addClass('hidden');
+    $(`[data-section="${to}"]`).removeClass('hidden');
+});
+
+$(document).on('click', '[data-to-tab]', e => {
+    const $el = $(e.currentTarget);
+    $('[data-to-tab].selected').removeClass('selected');
+    $el.addClass('selected');
+
+    const to = $el.attr('data-to-tab');
+    $('[data-tab]').addClass('hidden');
+    $(`[data-tab="${to}"]`).removeClass('hidden');
+});
+
+$(document).on('click', '[data-to-project]', e => {
+    // const project = $el.attr('data-to-project');
+    $('[data-section]').addClass('hidden');
+    $(`[data-section="project"]`).removeClass('hidden');
 });
